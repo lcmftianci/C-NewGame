7f32c378907fd9956fdbc1fa0a12140073f6d9f7
lcmftianci <707010543@qq.com>
2017-05-17 20:51:54 +0800

    C-GameNew=Version

---
+++ App1/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace App1
+{
+	[Activity(Label = "App1", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+        private Button btn;
+        int count = 0;
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            btn = FindViewById<Button>(Resource.Id.button1);
+            //btn.SetOnClickListener()
+            //这是什么东西
+            btn.Click += delegate { btn.Text = string.Format("{0} click", count++); };
+        }
+	}
+}
+
